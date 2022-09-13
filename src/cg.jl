ac18e88755067d429c239f6cdd36f2f1f0ef373a
Vincent Le Chenadec <vincentlechenadec@gmail.com>
2022-09-13 20:38:07 +0200

    Added history vector to cg! in order to log convergence information

--- src/cg.jl
+++ src/cg.jl
@@ -19,28 +19,34 @@ default_history(args...) = mapreduce(eltype, promote_type, args)[]
 
 """
     cg!
-    The conjugate gradient method 'CG' iteratively solves a linear system with a positive definite (and hence symmetric or Hermitian) coefficient matrix or operator (A) and searches for the optimal 'x' in a Krylov subspace of maximal size 'maxiter' or stops when 'norm(A*x - b) < tol'.
-    If no preconditioner is supplied, indentity matrix is used.
-    The default 'verbose' level is zero, which means no printed output.
+
+The conjugate gradient method 'CG' iteratively solves a linear system
+with a positive definite (and hence symmetric or Hermitian) coefficient
+matrix or operator (A) and searches for the optimal 'x' in a Krylov
+subspace of maximal size 'maxiter' or stops when 'norm(A*x - b) < tol'.
+If no preconditioner is supplied, indentity matrix is used.
+The default 'verbose' level is zero, which means no printed output.
+
+# Reference
+
+Dolean, Jolivet & Nataf 2015 - page 97 - Algorithm 3.2
+equivalent to Saad 2003 Page 259 - Algorithm 9.1
 
 """
-# Dolean, Jolivet & Nataf 2015 - page 97 - Algorithm 3.2
-# equivalent to Saad 2003 Page 259 - Algorithm 9.1 
 function cg!(
-    args...;
+    x, A, b,
+    ws=CGWs(x, A, b),
     Pr=I,
-    ws=CGWs(args...),
-    tol=default_tol(args...),
-    maxiter=default_maxiter(args...),
-    history=default_history(args...))
+    history=default_history(x, A, b),
+    tol=default_tol(x, A, b),
+    maxiter=default_maxiter(x, A, b))
 
-    x, A, b = args
     r, z, p, q = ws.buffer
 
-    copy!(r, b)               # r = b
-    mul!(r, A, x, -1., true)  # r = b - A * x
-    ldiv!(z, Pr, r)           # z = Pr \ r
-    copy!(p, z)               # p = z
+    copy!(r, b)                # r = b
+    mul!(r, A, x, -true, true) # r = b - A * x
+    ldiv!(z, Pr, r)            # z = Pr \ r
+    copy!(p, z)                # p = z
 
     empty!(history)
     push!(history, norm(z))
@@ -48,14 +54,14 @@ function cg!(
     while length(history) ≤ maxiter &&
           last(history) ≥ tol
 
-        ρ = dot(r, z)         # ρ= (r, z)
-        mul!(q, A, p)         # q = A * p
-        α = ρ / dot(p, q)     # α = ρ / (p, q)
-        axpy!(+α, p, x)       # x = x + α * p
-        axpy!(-α, q, r)       # r = r - α * q
-        ldiv!(z, Pr, r)       # z = Pr \ r
-        β = dot(r, z) / ρ     # β = (r, z) / ρ
-        axpby!(true, z, β, p) # p = z + β * p
+        ρ = dot(r, z)          # ρ= (r, z)
+        mul!(q, A, p)          # q = A * p
+        α = ρ / dot(p, q)      # α = ρ / (p, q)
+        axpy!(+α, p, x)        # x = x + α * p
+        axpy!(-α, q, r)        # r = r - α * q
+        ldiv!(z, Pr, r)        # z = Pr \ r
+        β = dot(r, z) / ρ      # β = (r, z) / ρ
+        axpby!(true, z, β, p)  # p = z + β * p
 
         push!(history, norm(z))
     end
